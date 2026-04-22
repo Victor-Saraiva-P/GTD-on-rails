3be73c9f88232207a6c9a5c7745e94124b6af138
Victor Alexandre Saraiva Pimentel <victoralex07062005@gmail.com>
2026-04-21 21:32:24 -0300

    chore: add desktop api client foundation

---
+++ apps/desktop/src/lib/api/apiClient.ts
@@ -0,0 +1,37 @@
+import { buildApiUrl } from "../../config/env";
+
+export class ApiRequestError extends Error {
+  readonly status: number;
+  readonly responseBody: string;
+
+  constructor(status: number, responseBody: string, message?: string) {
+    super(message ?? `API request failed with status ${status}`);
+    this.name = "ApiRequestError";
+    this.status = status;
+    this.responseBody = responseBody;
+  }
+}
+
+export async function apiFetch(pathname: string, init: RequestInit = {}) {
+  const response = await fetch(buildApiUrl(pathname), {
+    ...init,
+    headers: {
+      Accept: "application/json",
+      ...init.headers
+    }
+  });
+
+  if (!response.ok) {
+    const responseBody = await response.text();
+
+    throw new ApiRequestError(response.status, responseBody);
+  }
+
+  return response;
+}
+
+export async function apiJson<T>(pathname: string, init?: RequestInit): Promise<T> {
+  const response = await apiFetch(pathname, init);
+
+  return response.json() as Promise<T>;
+}
